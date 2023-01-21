6270cb8157b2dfe83025b3bb030e36439133d479
quochuy2k3 <voquochuy3006@gmail.com>
2023-01-21 14:51:39 +0700

    fix

---
+++ config.js
@@ -0,0 +1,8 @@
+const nameGirl = 'Bảo Khanh';
+const giftUrl = 'https://www.facebook.com/miumiubbkn';
+const eventName = 'Chúc mừng năm mới';
+const titleCard = 'Tặng Cậu';
+const contentCard = 'Chúc cậu được toại nguuyện mọi ước muốn, mỗi bước đường đều bằng phẳng, nhiều điều vui, mãi bình an. Thêm cái nữa, m4 hay m5 cậu rảnh thì cho phép tớ mời cậu đi chơi nhé';
+const giftImage = 'bk.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
